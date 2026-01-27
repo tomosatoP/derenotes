db401b733da10fba31b6a66af2dbdb4c53a71576
tomosatoP <tomosato.g@gmail.com>
2026-01-27 16:33:32 +0900

    Sphinx workflow: add sys.path configuration for GitHub actions

--- docs/api/source/conf.py
+++ docs/api/source/conf.py
@@ -3,6 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- sys path ----------------------------------------------------------------
+# 絶対パスで指定する。 GitHub actions に必要。
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path("../../../").resolve()))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
