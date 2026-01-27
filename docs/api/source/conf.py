07f693d47d2231336791751e4fadc61ca5aa1815
tomosatoP <tomosato.g@gmail.com>
2026-01-27 16:23:54 +0900

    Sphinx workflow: install additional system dependencies and remove requirements.txt

--- docs/api/source/conf.py
+++ docs/api/source/conf.py
@@ -3,13 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- sys path ----------------------------------------------------------------
-# 絶対パスで指定する。
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path("../../../").resolve()))
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
